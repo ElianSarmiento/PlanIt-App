d76e170277fa98106192286a2e0fcb1813625d93
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-05-12 13:59:08 -0400

    adjusted folder structure-assets; refactored image links on HTML

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+
 # from flask_sqlalchemy import SQLAlchemy # importing for database setup
 # from dotenv import load_dotenv # importing from dotenv package
 # import os
@@ -7,21 +8,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = (
-#     f"postgresql://postgres:{DB_PASSWORD}@localhost:{PORT}/bmi_db"    
+#     f"postgresql://postgres:{DB_PASSWORD}@localhost:{PORT}/bmi_db"
 # )
 # db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
-  
+
+
 # class Data(db.model):
 #     __tablename__ = "usertasks"
 #     id = db.Column(db.Integer, primary_key=True)
 #     task = db.Column(db.String(256))
 #     category = db.Column(db.String(128), unique=True)
 #     priority = db.Column(db.String(128))
-    
+
 #     def __init__(self, ptask, pcategory, ppriority):
 #       self.task = ptask
 #       self.category = pcategory
@@ -36,7 +39,7 @@ def index():
 #     task_info = request.form["task-box"]
 #     task_category = request.form["task-box"]
 #     task_priority = request.form["task-box"]
-    
+
 #     return render_template(
 #       task_info,
 #       task_category,
@@ -44,7 +47,5 @@ def index():
 #     )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
